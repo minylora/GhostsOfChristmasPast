8b055f3a668674813cc6aaea42811576178c8112
Mindy Lorance <mindy.y.lorance@gmail.com>
2021-10-05 17:01:47 -0700

    week 2 tests pass and gold stars acquired

--- Year_2020_Events/week2/test_week2.py
+++ Year_2020_Events/week2/test_week2.py
@@ -4,11 +4,11 @@ from . import week2
 
 part_one_valid_password_counter_test_cases = [
     (["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc", "2-9 c: cccccccccc"], 2),
-    ([], None),
+    ([], 0),
 ]
 part_two_valid_password_counter_test_cases = [
-    (["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc", "2-9 c: testc"], 1),
-    ([], None),
+    (["1-3 x: a", "2-5 x: xaxxb", "2-9 x: axcdefghx", "2-9 x: abcdefghx", "1-3 x: x"], 2),
+    ([], 0),
 ]
 
 
@@ -23,4 +23,4 @@ def test_part_one_valid_password_counter(pwdb_list, expected_result):
     "pwdb_list,expected_result", part_two_valid_password_counter_test_cases
 )
 def test_part_two_valid_password_counter(pwdb_list, expected_result):
-    assert week2.part_one_valid_password_counter(pwdb_list) == expected_result
+    assert week2.part_two_valid_password_counter(pwdb_list) == expected_result
--- Year_2020_Events/week2/week2.py
+++ Year_2020_Events/week2/week2.py
@@ -6,35 +6,39 @@ from Year_2020_Events.myutils.myutils import get_str_list
 
 # Part 1
 def part_one_valid_password_counter(pwdb: List[str]) -> int:
-    if pwdb:
-        num_valid_passwords = 0
-        for line in pwdb:
-            values = re.search(r"(\d+)-(\d+)\s(\w):\s(\w+)", line)
-            num_policy_occurances = values.group(4).count(values.group(3))
-            if int(values.group(1)) <= num_policy_occurances <= int(values.group(2)):
-                num_valid_passwords += 1
-        return num_valid_passwords
+    num_valid_passwords = 0
+    for line in pwdb:
+        values = re.search(r"(\d+)-(\d+)\s(\w):\s(\w+)", line)
+        num_policy_occurances = values.group(4).count(values.group(3))
+        if int(values.group(1)) <= num_policy_occurances <= int(values.group(2)):
+            num_valid_passwords += 1
+    return num_valid_passwords
 
 
 # Part 2
 def part_two_valid_password_counter(pwdb: List[str]) -> int:
-    if pwdb:
-        num_valid_passwords = 0
-        for line in pwdb:
-            values = re.search(r"(\d+)-(\d+)\s(\w):\s(\w+)", line)
-            policy_indx_1 = int(values.group(1))-1
-            policy_indx_2 = int(values.group(2))-1
-            policy = values.group(3)
-            password = values.group(4)
-            if password.count(policy) == 1 and password.find(policy) == policy_indx_1 or password.find(policy) == policy_indx_2:
+    num_valid_passwords = 0
+    for line in pwdb:
+        values = re.search(r"(\d+)-(\d+)\s(\w):\s(\w+)", line)
+        policy_indx_1 = int(values.group(1)) - 1
+        policy_indx_2 = int(values.group(2)) - 1
+        policy = values.group(3)
+        password = values.group(4)
+        value_two = None
+        if len(password) > policy_indx_2:
+            if password[policy_indx_1] != password[policy_indx_2] and policy in (password[policy_indx_1], password[policy_indx_2]):
                 num_valid_passwords += 1
-        return num_valid_passwords
+        else:
+            if policy == password[policy_indx_1]:
+                num_valid_passwords += 1
+    return num_valid_passwords
 
 
 def main():
     pwdb_list = get_str_list("input.txt")
     part_one_ans = part_one_valid_password_counter(pwdb_list)
     print(part_one_ans)
+
     part_two_ans = part_two_valid_password_counter(pwdb_list)
     print(part_two_ans)
 
