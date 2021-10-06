c2db7537af6334553eab9b3bc2f8125c4a709616
Mindy Lorance <mindy.y.lorance@gmail.com>
2021-10-05 20:41:41 -0700

    init

---
+++ Year_2019_Events/week2/test_problem2.py
@@ -0,0 +1,6 @@
+import pytest
+from week2.week2 import *
+
+
+def test_part_one():
+    pass
---
+++ Year_2019_Events/week2/week2.py
@@ -0,0 +1,14 @@
+from typing import List
+from math import floor
+
+
+def part_one_():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
