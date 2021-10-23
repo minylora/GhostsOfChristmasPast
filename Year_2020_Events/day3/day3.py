666b4cd45a487d56fbaa492ebebf87ccd205688d
Mindy Lorance <mindy.y.lorance@gmail.com>
2021-10-22 19:11:09 -0600

    test passing, second gold start

--- Year_2020_Events/day3/day3.py
+++ Year_2020_Events/day3/day3.py
@@ -5,22 +5,44 @@ from Year_2020_Events.myutils.myutils import get_str_list
 
 
 # Part 1
-def how_many_tress_for_part_one(slope_map: List[str]) -> int:
-    num_tress = 0
-    if slope_map[0][0] == "#":
-        num_tress += 1
-    for row in range(1, len(slope_map)):
-        pos = (row - 1) * 3 + 3
+def how_many_trees_for_part_one(slope_map: List[str]) -> int:
+    num_trees = 0
+    for row in range(0, len(slope_map)):
+        pos = (row * 3) % len(slope_map[row])
+        if slope_map[row][pos] == "#":
+            num_trees += 1
+    return num_trees
+
+
+# Part 2 - Find a generic part 1, then use to solve for mixed slopes
+def how_many_trees_for_single_slope(slope_map: List[str], right: int, down: int) -> int:
+    num_trees = 0
+    count = 0
+    for row in range(0, len(slope_map), down):
+        pos = count * right
         pos = pos % len(slope_map[row])
         if slope_map[row][pos] == "#":
-            num_tress += 1
-    return num_tress
+            num_trees += 1
+        count += 1
+    return num_trees
+
+
+def how_many_trees_for_multiple_slopes_multiplied(slope_map: List[str], slopes: List[tuple]):
+    num_trees_multiplied = 1
+    for right, down in slopes:
+        num_trees_for_slope = how_many_trees_for_single_slope(slope_map, right, down)
+        num_trees_multiplied = num_trees_multiplied*num_trees_for_slope
+    return num_trees_multiplied
 
 
 def main():
     slope_map = get_str_list("input.txt")
-    num_trees = how_many_tress_for_part_one(slope_map)
+    num_trees = how_many_trees_for_part_one(slope_map)
     print(f"part_one: {num_trees}")
+
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    num_trees_mult = how_many_trees_for_multiple_slopes_multiplied(slope_map, slopes)
+    print(f"part_two: {num_trees_mult}")
 
 
 if __name__ == "__main__":
--- Year_2020_Events/day3/test_day3.py
+++ Year_2020_Events/day3/test_day3.py
@@ -22,9 +22,42 @@ def slope_map_input():
     return get_str_list(filename)
 
 
-def test_how_many_tress_for_part_one_with_sample_input(slope_map_sample):
-    assert day3.how_many_tress_for_part_one(slope_map_sample) == 7
+@pytest.fixture
+def sample_slopes():
+    return [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
 
 
-def test_how_many_tress_for_part_one_with_input(slope_map_input):
-    assert day3.how_many_tress_for_part_one(slope_map_input) == 187
+how_many_trees_part_one_data = [
+    ('slope_map_sample', 7),
+    ('slope_map_input', 187),
+]
+
+
+how_many_trees_given_slope_data = [
+    ('slope_map_sample', 3, 1, 7),
+    ('slope_map_input', 3, 1, 187),
+    ('slope_map_sample', 1, 1, 2),
+    ('slope_map_sample', 5, 1, 3),
+    ('slope_map_sample', 7, 1, 4),
+    ('slope_map_sample', 1, 2, 2),
+]
+
+
+@pytest.mark.parametrize("slope_map, result", how_many_trees_part_one_data)
+def test_how_many_trees_for_part_one_with_sample(slope_map, result, request):
+    slope_map = request.getfixturevalue(slope_map)
+    assert day3.how_many_trees_for_part_one(slope_map) == result
+
+
+@pytest.mark.parametrize("slope_map, right, down, result", how_many_trees_given_slope_data)
+def test_how_many_trees_for_single_slope_sample(slope_map, right, down, result, request):
+    slope_map = request.getfixturevalue(slope_map)
+    assert day3.how_many_trees_for_single_slope(slope_map, right, down) == result
+
+
+def test_how_many_trees_for_multiple_slopes(slope_map_sample, sample_slopes):
+    assert day3.how_many_trees_for_multiple_slopes_multiplied(slope_map_sample, sample_slopes) == 336
+
+
+def test_how_many_trees_for_multiple_slopes(slope_map_input, sample_slopes):
+    assert day3.how_many_trees_for_multiple_slopes_multiplied(slope_map_input, sample_slopes) == 4723283400
