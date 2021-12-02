9cdf1f17a3c500c2e496f0d9fd05f244d429907a
Mindy Lorance <mindy.y.lorance@gmail.com>
2021-12-01 18:01:08 -0700

    initial day 1 answers with gold stars

---
+++ Year_2021_Events/day1/day1.py
@@ -0,0 +1,32 @@
+from typing import List
+
+from Year_2021_Events.myutils.myutils import get_int_list
+
+
+def get_sliding_window(measurements: List[int]) -> List[int]:
+    sliding_window_values = []
+    for i in range(0, len(measurements)-2):
+        win = measurements[i] + measurements[i+1] + measurements[i+2]
+        sliding_window_values.append(win)
+    return sliding_window_values
+
+
+def how_many_are_larger_than_the_previous(measurements: List[int]) -> int:
+    count = 0
+    for i in range(0, len(measurements) - 1):
+        diff = measurements[i] - measurements[i + 1]
+        if diff < 0:
+            count += 1
+    return count
+
+
+def main():
+    measurements = get_int_list("day1_input.txt")
+    part_one = how_many_are_larger_than_the_previous(measurements)
+    print(part_one)
+    window = get_sliding_window(measurements)
+    part_two = how_many_are_larger_than_the_previous(window)
+    print(part_two)
+
+if __name__ == "__main__":
+    main()
--- Year_2021_Events/day1/test_day1.py
+++ Year_2021_Events/day1/test_day1.py
@@ -1,4 +1,19 @@
 import pytest
 from . import day1
 
-# tests
+test_measurements = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+expected_sliding_window_values = [607, 618, 618, 617, 647, 716, 769, 792]
+
+
+def test_part_one():
+    expected_result = 7
+    assert day1.how_many_are_larger_than_the_previous(test_measurements) == expected_result
+
+
+def test_part_two_sliding_win_math():
+    assert day1.get_sliding_window(test_measurements) == expected_sliding_window_values
+
+
+def test_part_two():
+    expected_result = 5
+    assert day1.how_many_are_larger_than_the_previous(expected_sliding_window_values) == expected_result
