b6f90dc7b9e8f2a64a7b21832dcdb8c478c789f8
Mindy Lorance <mindy.y.lorance@gmail.com>
2021-12-06 16:40:24 -0700

    init commit

---
+++ Year_2021_Events/day6/day6.py
@@ -0,0 +1,9 @@
+from Year_2020_Events.myutils.myutils import get_str_list
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ Year_2021_Events/day6/test_day5.py
@@ -0,0 +1,6 @@
+import pytest
+from . import day6
+
+
+def test_one():
+    assert True
