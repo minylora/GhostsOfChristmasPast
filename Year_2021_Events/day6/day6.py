e35c554e8b543e12be9e70fa4f28fc2b8fd43885
Mindy Lorance <mindy.y.lorance@gmail.com>
2021-12-06 17:53:24 -0700

    part one done; wip fish tracker

--- Year_2021_Events/day6/day6.py
+++ Year_2021_Events/day6/day6.py
@@ -1,8 +1,64 @@
-from Year_2020_Events.myutils.myutils import get_str_list
+from typing import List
+
+
+def get_initial_state(filename: str) -> List[int]:
+    f = open(filename, "r")
+    timer = f.readline().split(",")
+    f.close()
+    timer = [int(i) for i in timer]
+    return timer
+
+
+# def how_many_lantern_fish(init_state: List[int], days: int) -> int:
+#     if days < 0:
+#         raise ValueError
+#     if days == 0:
+#         return len(init_state)
+#
+#     current_state = init_state
+#     for day in range(1, days+1):
+#         next_state = [None]*len(current_state)
+#         new_fish = 0
+#         for i in range(0, len(current_state)):
+#             next_state[i] = current_state[i] - 1
+#             if next_state[i] < 0:
+#                 next_state[i] = 6
+#                 new_fish += 1
+#         for i in range(0, new_fish):
+#             next_state.append(8)
+#         current_state = next_state
+#
+#     return len(current_state)
+
+
+def how_many_lantern_fish(init_state: List[int], days: int) -> int:
+    if days < 0:
+        raise ValueError
+    if days == 0:
+        return len(init_state)
+
+    # create fish tracker
+    init_state.sort()
+    f = 0
+    fish_tracker = [[init_state[0], 1]]
+    for i in range(1, len(init_state)):
+        if fish_tracker[f][0] == init_state[i]:
+            fish_tracker[f][1] += 1
+        else:
+            fish_tracker.append([init_state[i], 1])
+            f += 1
+
+
+
+    return 1
 
 
 def main():
-    pass
+    init_fish = get_initial_state("mini_state")
+    day_one = how_many_lantern_fish(init_fish, 80)
+    print(day_one)
+    # day_two = how_many_lantern_fish(init_fish, 256)
+    # print(day_two)
 
 
 if __name__ == "__main__":
--- Year_2021_Events/day6/test_day5.py
+++ Year_2021_Events/day6/test_day5.py
@@ -1,6 +1,18 @@
 import pytest
 from . import day6
 
+mini_state = r"C:\Users\Mindy\Documents\CodeyCode\GhostsOfChristmasPast\Year_2021_Events\day6\mini_state"
+mini_list = [3, 4, 3, 1, 2]
+days = [0, 1, 2, 10, 18, 80, 256]
+totals = [5, 5, 6, 12, 26, 5934, 26984457539]
+expected = zip(days, totals)
 
-def test_one():
-    assert True
+
+def test_get_initial_state():
+    assert day6.get_initial_state(mini_state) == mini_list
+
+
+@pytest.mark.parametrize("day, expected_total", expected)
+def test_how_many_lantern_fish(day, expected_total):
+    assert day6.how_many_lantern_fish(mini_list, day) == expected_total
+
