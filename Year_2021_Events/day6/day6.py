a296010bb9c8f294b9ddd3c1efa77e07a049b01f
Mindy Lorance <mindy.y.lorance@gmail.com>
2021-12-06 18:24:33 -0700

    part 2 done

--- Year_2021_Events/day6/day6.py
+++ Year_2021_Events/day6/day6.py
@@ -9,6 +9,7 @@ def get_initial_state(filename: str) -> List[int]:
     return timer
 
 
+## part one; no optimization
 # def how_many_lantern_fish(init_state: List[int], days: int) -> int:
 #     if days < 0:
 #         raise ValueError
@@ -38,27 +39,41 @@ def how_many_lantern_fish(init_state: List[int], days: int) -> int:
         return len(init_state)
 
     # create fish tracker
-    init_state.sort()
-    f = 0
-    fish_tracker = [[init_state[0], 1]]
-    for i in range(1, len(init_state)):
-        if fish_tracker[f][0] == init_state[i]:
-            fish_tracker[f][1] += 1
-        else:
-            fish_tracker.append([init_state[i], 1])
-            f += 1
+    fish_tracker = []
+    for i in range(9):
+        empty_fish = [i, 0]
+        fish_tracker.append(empty_fish)
 
+    # update fish tracker to init state
+    for state in init_state:
+        fish_tracker[state][1] += 1
 
+    # update fish tracker over number of days
+    for day in range(0, days):
+        # store how many fish are at 0
+        new_fish = fish_tracker[0][1]
 
-    return 1
+        # number of days is reduced by one
+        for state in range(0, 8):
+            fish_tracker[state][1] = fish_tracker[state+1][1]
+
+        # use stored fish count to reset fish and to create new fish
+        fish_tracker[6][1] += new_fish
+        fish_tracker[8][1] = new_fish
+
+    # calculate number of fish tracked
+    fish_count = 0
+    for state in fish_tracker:
+        fish_count += state[1]
+    return fish_count
 
 
 def main():
-    init_fish = get_initial_state("mini_state")
+    init_fish = get_initial_state("day6_input.txt")
     day_one = how_many_lantern_fish(init_fish, 80)
     print(day_one)
-    # day_two = how_many_lantern_fish(init_fish, 256)
-    # print(day_two)
+    day_two = how_many_lantern_fish(init_fish, 256)
+    print(day_two)
 
 
 if __name__ == "__main__":
