9aa8cc2fb8d1d424d29ce2f9ac59ad627240f3fa
Mindy Lorance <mindy.y.lorance@gmail.com>
2021-12-12 10:40:27 -0700

    day 11 init

---
+++ Year_2021_Events/day11/day11.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from Year_2020_Events.myutils.myutils import get_int_list
+
+
+# when a step is taken
+#      - first everything increases by 1
+#      - then greater than 9 flashes - flash resets energy to 0
+#      - all adjacent, including diagonals, increase by 1
+#      - then greater than 9 flashes - flash resets energy to 0
+#      - each octopus can only flash once in a step
+
+def m():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ Year_2021_Events/day11/test_day11.py
@@ -0,0 +1,5 @@
+import pytest
+from . import day11
+
+def test():
+    pass
